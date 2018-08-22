706f46d529854004a1d53b731f7700c7551a18b4
albyn0s <dmitryneff@gmail.com>
2018-08-23 01:40:11 +0500

    Revert "Add files via upload"
    This reverts commit d9ee61618d51036914406bc565e5d3421f245c9f.

--- Client/App.xaml.cs
+++ Client/App.xaml.cs
@@ -1,5 +1,4 @@
-<<<<<<< HEAD
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
@@ -16,22 +15,3 @@ namespace WPF_application
     {
     }
 }
-=======
-﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Linq;
-using System.Threading.Tasks;
-using System.Windows;
-
-namespace WPF_application
-{
-    /// <summary>
-    /// Логика взаимодействия для App.xaml
-    /// </summary>
-    public partial class App : Application
-    {
-    }
-}
->>>>>>> 5bd1de91cf308b3afa5db67b230b886b6fce0121
--- Client/MainWindow.xaml.cs
+++ Client/MainWindow.xaml.cs
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -52,58 +51,3 @@ namespace WPF_application
         }
     }
 }
-=======
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-using System.Collections.ObjectModel;
-using System.Data.OleDb;
-using System.Data.SqlClient;
-using System.Configuration;
-using System.Data;
-using WPF_application.ViewModel;
-using System.Net.Http;
-using System.Net.Http.Headers;
-
-namespace WPF_application
-{
-    /// <summary>
-    /// Логика взаимодействия для MainWindow.xaml
-    /// </summary>
-    public partial class MainWindow : Window
-    {
-     public static  MainWindowViewModel ViewModel = new MainWindowViewModel();
-
-        public MainWindow()
-        {        
-            InitializeComponent();
-        }
-
-        private void Window_Loaded(object sender, RoutedEventArgs e)
-        {
-            DataContext = ViewModel;
-        }
-
-        private void Button_Click(object sender, RoutedEventArgs e)
-        {
-
-        }
-
-        private void Button_Click_1(object sender, RoutedEventArgs e)
-        {
-
-        }
-    }
-}
->>>>>>> 5bd1de91cf308b3afa5db67b230b886b6fce0121
